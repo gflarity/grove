2390d60b60859e05305cd3c0f3a4a5b2f40f5597
Geoff Flarity <gflarity@nvidia.com>
2025-09-30 16:43:07 -0400

    rename ci to e2e_testing
    Signed-off-by: Geoff Flarity <gflarity@nvidia.com>

--- operator/e2e_testing/tests/e2e_test.go
+++ operator/e2e_testing/tests/e2e_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/NVIDIA/grove/operator/ci/utils"
+	"github.com/NVIDIA/grove/operator/e2e_testing/utils"
 	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
